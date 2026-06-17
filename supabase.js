c914c9a58b3afbf4d125480dc73db14b80bc8304
investormachuki-bit <investormachuki@gmail.com>
2026-06-17 14:24:06 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const supabaseClient = supabase.createClient(
-  https://osnrnrgnegqpbiknsgit.supabase.co,
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zbnJucmduZWdxcGJpa25zZ2l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2ODI2MTUsImV4cCI6MjA5NzI1ODYxNX0._zOqjvIhaRCwGHBpEY1q4guGxMmXOATFj4F4rEF-dYc
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
