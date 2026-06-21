00af14ff9687c9434dc8a7eeb6a94e333a875684
investormachuki-bit <investormachuki@gmail.com>
2026-06-22 00:44:25 +0300

    Update flow-builder.js

--- flow-builder.js
+++ flow-builder.js
@@ -1,43 +1,47 @@
-let nodes = [];
-let count = 1;
+window.onload = function () {
+  let nodes = [];
+  let count = 1;
 
-const app = document.getElementById("app");
+  const app = document.getElementById("app");
+  const addMessageBtn = document.getElementById("addMessage");
+  const addQuestionBtn = document.getElementById("addQuestion");
 
-function render() {
-  app.innerHTML = "";
+  function render() {
+    app.innerHTML = "";
 
-  nodes.forEach((node) => {
-    const div = document.createElement("div");
-    div.innerText = node.label;
-    div.style.padding = "15px";
-    div.style.margin = "10px";
-    div.style.border = "2px solid #333";
-    div.style.borderRadius = "8px";
-    div.style.width = "180px";
-    div.style.background = "#fff";
-    div.style.cursor = "pointer";
+    nodes.forEach((node) => {
+      const div = document.createElement("div");
+      div.innerText = node.label;
+      div.style.padding = "15px";
+      div.style.margin = "10px";
+      div.style.border = "2px solid #333";
+      div.style.borderRadius = "8px";
+      div.style.width = "180px";
+      div.style.background = "#fff";
+      div.style.cursor = "pointer";
 
-    app.appendChild(div);
-  });
-}
+      app.appendChild(div);
+    });
+  }
 
-function addNode(type) {
-  nodes.push({
-    id: count,
-    type,
-    label: `${type} ${count}`,
-  });
+  function addNode(type) {
+    nodes.push({
+      id: count,
+      type: type,
+      label: type + " " + count
+    });
 
-  count++;
+    count++;
+    render();
+  }
+
+  addMessageBtn.onclick = function () {
+    addNode("Message");
+  };
+
+  addQuestionBtn.onclick = function () {
+    addNode("Question");
+  };
+
   render();
-}
-
-document.getElementById("addMessage").addEventListener("click", function () {
-  addNode("Message");
-});
-
-document.getElementById("addQuestion").addEventListener("click", function () {
-  addNode("Question");
-});
-
-render();
+};
